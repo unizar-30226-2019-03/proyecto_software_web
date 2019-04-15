352ae5ff88039dc7639e7d849cff90dabfc92600
David <davidsolanas086@gmail.com>
2019-04-15 17:08:23 +0200

    Página de Rankings

--- interfaz/src/components/Rankings.jsx
+++ interfaz/src/components/Rankings.jsx
@@ -1,14 +1,149 @@
 import React, { Component } from "react";
 import BarraNavegacion from "./BarraNavegacion";
 import { Helmet } from "react-helmet";
+import { ListGroup, Dropdown, Button, FormControl } from "react-bootstrap";
+import Link from "react-router-dom/Link";
+import foto from "../assets/favicon.ico";
+import CustomToggle from "./CustomToggle";
+import { FaTrophy } from "react-icons/fa";
+
+const asignaturas = [
+  {
+    nombre: "Aprendizaje Automático",
+    uni: "Universidad de Zaragoza",
+    foto: foto,
+    puntuacion: "98%",
+    posicion: 1
+  },
+  {
+    nombre: "Proyecto Software",
+    uni: "Universidad de Zaragoza",
+    foto: foto,
+    puntuacion: "95%",
+    posicion: 2
+  },
+  {
+    nombre: "Bases de Datos I",
+    uni: "Universidad de Zaragoza",
+    foto: foto,
+    puntuacion: "90%",
+    posicion: 3
+  },
+  {
+    nombre: "Bases de Datos II",
+    uni: "Universidad de Zaragoza",
+    foto: foto,
+    puntuacion: "85%",
+    posicion: 4
+  },
+  {
+    nombre: "Administración de Sistemas I",
+    uni: "Universidad de Zaragoza",
+    foto: foto,
+    puntuacion: "82%",
+    posicion: 5
+  },
+  {
+    nombre: "Administración de Sistemas II",
+    uni: "Universidad de Zaragoza",
+    foto: foto,
+    puntuacion: "80%",
+    posicion: 6
+  },
+  {
+    nombre: "Inteligencia Artificial",
+    uni: "Universidad de Zaragoza",
+    foto: foto,
+    puntuacion: "78%",
+    posicion: 7
+  },
+  {
+    nombre: "Ingeniería del Software",
+    uni: "Universidad de Zaragoza",
+    foto: foto,
+    puntuacion: "77%",
+    posicion: 8
+  },
+  {
+    nombre: "Programación de Sistemas Concurrentes y Distribuidos",
+    uni: "Universidad de Zaragoza",
+    foto: foto,
+    puntuacion: "73%",
+    posicion: 9
+  }
+];
+
+const ItemAsignatura = ({ nombre, uni, foto, pos, trofeo, puntuacion }) => {
+  return (
+    <Link to="/asig/1" style={{ color: "black", textDecoration: "none" }}>
+      <ListGroup.Item className="fondo">
+        <p className="puestoRanking">
+          {pos}.{" "}
+          {trofeo !== null ? (
+            <FaTrophy style={{ marginLeft: "10px" }} color={trofeo} size={25} />
+          ) : null}
+        </p>
+        <p className="asigRank">{nombre}</p>
+        <img className="imagenRank" src={foto} alt="imagen asinatura" />
+        <p className="uniRank">{uni}</p>
+        <p className="puntuacion">{puntuacion}</p>
+      </ListGroup.Item>
+    </Link>
+  );
+};
+
+const ListaAsignaturas = lista =>
+  lista.map(el => {
+    const { nombre, uni, foto, puntuacion, posicion } = el;
+    let trofeo = null;
+    switch (posicion) {
+      case 1:
+        trofeo = "#D4AF37";
+        break;
+      case 2:
+        trofeo = "#C0C0C0";
+        break;
+      case 3:
+        trofeo = "#CD7F32";
+        break;
+
+      default:
+        break;
+    }
+    return (
+      <ItemAsignatura
+        nombre={nombre}
+        uni={uni}
+        foto={foto}
+        key={nombre}
+        pos={posicion}
+        trofeo={trofeo}
+        puntuacion={puntuacion}
+      />
+    );
+  });
 
 class Rankings extends Component {
   constructor() {
     super();
     this.state = {
-      contentMargin: "300px"
+      contentMargin: "300px",
+      filtro: ""
     };
     this.handleChange = this.handleChange.bind(this);
+    this.cambiaFiltro = this.cambiaFiltro.bind(this);
+    this.filtrar = this.filtrar.bind(this);
+  }
+
+  cambiaFiltro(e) {
+    this.setState({ filtro: e.target.value.toLowerCase().trim() });
+  }
+
+  filtrar(lista) {
+    let asig = lista.filter(
+      a => "" || a.nombre.toLowerCase().startsWith(this.state.filtro)
+    );
+    return asig;
   }
 
   handleChange(display) {
@@ -19,6 +154,8 @@ class Rankings extends Component {
     }
   }
   render() {
+    const asignaturasFiltradas = this.filtrar(asignaturas);
+    const listaAsign = ListaAsignaturas(asignaturasFiltradas);
     return (
       <div>
         <Helmet>
@@ -39,7 +176,33 @@ class Rankings extends Component {
             marginTop: "80px"
           }}
         >
-          RANKINGS PAGE
+          <div className="listado-asignaturas">
+            <h5>Ránking de Asignaturas</h5>
+
+            <ListGroup variant="flush" className="lista">
+              <Dropdown style={{ marginBottom: "5px" }} drop={"right"}>
+                <Dropdown.Toggle
+                  as={CustomToggle}
+                  id="dropdown-custom-components"
+                >
+                  <Button className="boton-filtro">
+                    Filtrar por asignaturas
+                  </Button>
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu style={{ marginTop: "-40px" }}>
+                  <FormControl
+                    autoFocus
+                    className="mx-3 my-2 w-auto"
+                    style={{ cursor: "text" }}
+                    placeholder="Escribe para filtrar..."
+                    onChange={this.cambiaFiltro}
+                  />
+                </Dropdown.Menu>
+              </Dropdown>
+              {listaAsign}
+            </ListGroup>
+          </div>
         </div>
       </div>
     );
