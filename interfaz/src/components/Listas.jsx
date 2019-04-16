3c7952994de7fa1227a2ff4adc714a8823a1cb52
David <davidsolanas086@gmail.com>
2019-04-16 19:22:44 +0200

    Página Mis Listas

--- interfaz/src/components/Listas.jsx
+++ interfaz/src/components/Listas.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import BarraNavegacion from "./BarraNavegacion";
 import { Helmet } from "react-helmet";
+import ListaHorizontal from "./ListaHorizontal";
+import { FaPlus, FaPencilAlt, FaRegTrashAlt } from "react-icons/fa";
 
 class Listas extends Component {
   constructor() {
@@ -39,7 +41,124 @@ class Listas extends Component {
             marginTop: "80px"
           }}
         >
-          MIS LISTAS PAGE
+          <div>
+            <div
+              style={{
+                width: "93.45%",
+                display: "flex",
+                borderBottom: "1px solid lightgrey",
+                marginBottom: "40px"
+              }}
+            >
+              <h5
+                style={{
+                  fontWeight: "bold"
+                }}
+              >
+                Mis listas de reproducción
+              </h5>
+              <div className="anyadir-lista" onClick={() => alert("Hola")}>
+                <div style={{ marginRight: "5px" }}>
+                  <FaPlus color={"#00000080"} />
+                </div>
+                <div style={{ marginTop: "2px", color: "#00000080" }}>
+                  NUEVA LISTA
+                </div>
+              </div>
+            </div>
+            <div>
+              <div style={{ display: "flex" }}>
+                <div style={{ marginRight: "15px" }}>
+                  <h6 style={{ fontWeight: "bold" }}>
+                    Lista de reproducción A
+                  </h6>
+                </div>
+                <div style={{ display: "flex", marginTop: "-4px" }}>
+                  <div
+                    style={{
+                      cursor: "pointer",
+                      marginRight: "15px"
+                    }}
+                  >
+                    <FaPencilAlt className="icono-lista" />
+                  </div>
+                  <div style={{ cursor: "pointer" }}>
+                    <FaRegTrashAlt className="icono-lista" />
+                  </div>
+                </div>
+              </div>
+              <ListaHorizontal />
+            </div>
+            <div>
+              <div style={{ display: "flex" }}>
+                <div style={{ marginRight: "15px" }}>
+                  <h6 style={{ fontWeight: "bold" }}>
+                    Lista de reproducción B
+                  </h6>
+                </div>
+                <div style={{ display: "flex", marginTop: "-4px" }}>
+                  <div
+                    style={{
+                      cursor: "pointer",
+                      marginRight: "15px"
+                    }}
+                  >
+                    <FaPencilAlt className="icono-lista" />
+                  </div>
+                  <div style={{ cursor: "pointer" }}>
+                    <FaRegTrashAlt className="icono-lista" />
+                  </div>
+                </div>
+              </div>
+              <ListaHorizontal />
+            </div>
+            <div>
+              <div style={{ display: "flex" }}>
+                <div style={{ marginRight: "15px" }}>
+                  <h6 style={{ fontWeight: "bold" }}>
+                    Lista de reproducción C
+                  </h6>
+                </div>
+                <div style={{ display: "flex", marginTop: "-4px" }}>
+                  <div
+                    style={{
+                      cursor: "pointer",
+                      marginRight: "15px"
+                    }}
+                  >
+                    <FaPencilAlt className="icono-lista" />
+                  </div>
+                  <div style={{ cursor: "pointer" }}>
+                    <FaRegTrashAlt className="icono-lista" />
+                  </div>
+                </div>
+              </div>
+              <ListaHorizontal />
+            </div>
+            <div>
+              <div style={{ display: "flex" }}>
+                <div style={{ marginRight: "15px" }}>
+                  <h6 style={{ fontWeight: "bold" }}>
+                    Lista de reproducción D
+                  </h6>
+                </div>
+                <div style={{ display: "flex", marginTop: "-4px" }}>
+                  <div
+                    style={{
+                      cursor: "pointer",
+                      marginRight: "15px"
+                    }}
+                  >
+                    <FaPencilAlt className="icono-lista" />
+                  </div>
+                  <div style={{ cursor: "pointer" }}>
+                    <FaRegTrashAlt className="icono-lista" />
+                  </div>
+                </div>
+              </div>
+              <ListaHorizontal />
+            </div>
+          </div>
         </div>
       </div>
     );
