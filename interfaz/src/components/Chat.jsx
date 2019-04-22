a9886f71d916ad1bbb5bf012d9cb0df2ee4ace51
paulalardies <38422377+paulalardies@users.noreply.github.com>
2019-04-22 17:29:12 +0200

    Update Chat.jsx

--- interfaz/src/components/Chat.jsx
+++ interfaz/src/components/Chat.jsx
@@ -3,9 +3,49 @@ import BarraNavegacion from "./BarraNavegacion";
 import { Helmet } from "react-helmet";
 import imagenUsuario from "../assets/user.png";
 import Messages from './Messages';
-import ChatInput from './ChatInput';
 
 require('../Chat.css');
+
+class ChatInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { chatInput: '' };
+
+    // React ES6 does not bind 'this' to event handlers by default
+    this.submitHandler = this.submitHandler.bind(this);
+    this.textChangeHandler = this.textChangeHandler.bind(this);
+  }
+  
+  submitHandler(event) {
+    // Stop the form from refreshing the page on submit
+    event.preventDefault();
+
+    // Clear the input box
+    this.setState({ chatInput: '' });
+
+    // Call the onSend callback with the chatInput message
+    this.props.onSend(this.state.chatInput);
+  }
+
+  textChangeHandler(event)  {
+    this.setState({ chatInput: event.target.value });
+  }
+
+  render() {
+    return (
+      <form className="chat-input" onSubmit={this.submitHandler}>
+        <input type="text"
+          onChange={this.textChangeHandler}
+          value={this.state.chatInput}
+          placeholder="Write a message..."
+          required />
+      </form>
+    );
+  }
+}
+
+ChatInput.defaultProps = {
+};
 
 class Chat extends Component {
   socket = {};
@@ -73,5 +113,6 @@ class Chat extends Component {
     );
   }
 }
+
 
 export default Chat;
