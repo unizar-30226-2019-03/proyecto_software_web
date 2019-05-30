5e9abba54b2f019b04fca7a7b012d8fa52e1e809
David Solanas <david@MacBook-Pro-de-David.local>
2019-05-30 19:21:57 +0200

    Control volumen video

--- app/src/components/Video.jsx
+++ app/src/components/Video.jsx
@@ -141,7 +141,7 @@ class Video extends Component {
             <CurrentTimeDisplay order={4.1} />
             <TimeDivider order={4.2} />
             <PlaybackRateMenuButton rates={[5, 2, 1, 0.5, 0.1]} order={7.1} />
-            <VolumeMenuButton disabled />
+            <VolumeMenuButton vertical />
           </ControlBar>
         </Player>
       </div>
